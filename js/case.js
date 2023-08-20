9657b56540b3b0a8d564b98c5b04a87dea6fecd5
shakhera <shakherakhanom024@gmail.com>
2023-08-20 22:16:11 +0600

    Updated Case count and amount

--- js/case.js
+++ js/case.js
@@ -19,3 +19,29 @@ document.getElementById('btn-case-minus').addEventListener('click', function () 
     caseNumberField.value = newCaseMinus;
 
 })
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const totalCaseNumberElement = document.getElementById('case-amount');
+    const totalCaseAmountString = totalCaseNumberElement.innerText;
+    const totalCaseAMount = parseFloat(totalCaseAmountString);
+    // console.log(totalCaseAmountString);
+    // console.log(typeof totalCaseAmountString);
+    // console.log(totalCaseAMount);
+    // console.log(typeof totalCaseAMount);
+
+    const newTotalCaseAmount = totalCaseAMount + 425;
+    totalCaseNumberElement.innerText = newTotalCaseAmount;
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    const totalCaseNumberElement = document.getElementById('case-amount');
+    const totalCaseAmountString = totalCaseNumberElement.innerText;
+    const totalCaseAMount = parseFloat(totalCaseAmountString);
+    // console.log(totalCaseAmountString);
+    // console.log(typeof totalCaseAmountString);
+    // console.log(totalCaseAMount);
+    // console.log(typeof totalCaseAMount);
+
+    const newTotalCaseAmount = totalCaseAMount - 425;
+    totalCaseNumberElement.innerText = newTotalCaseAmount;
+})
